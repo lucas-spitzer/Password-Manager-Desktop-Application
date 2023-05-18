b7cd5c22e1039128309dd6abee361e2de63bb22a
lucas-spitzer <124513353+lucas-spitzer@users.noreply.github.com>
2023-05-18 18:56:00 -0400

    Update setup2FA.py

--- twoFA/setup2FA.py
+++ twoFA/setup2FA.py
@@ -1,19 +1,29 @@
-import pyotp
-import qrcode
+import pyotp, qrcode, sys, os
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 
 def generate_key():
     ''' Generates a key to be written into a .txt file and used during "authentication_setup" and "verify_2FA_code" functions. '''
 
     KEY = pyotp.random_base32()  # Randomly generates a constant global variable as a key to be utilized in later in the script. 
-    with open('twoFA/key.txt', 'w') as file:  # Opens and writes key into 'key.txt' within the 'twoFA' folder.
+    with open(resource_path('TwoFA/key.txt'), 'w') as file:  # Opens and writes key into 'key.txt' within the 'TwoFA' folder.
         file.write(KEY)
 
 
 def retrieve_key():
     ''' Reads and returns key from "key.txt" file. '''
 
-    with open('twoFA/key.txt', 'r') as file:  # Opens and Reads 'key.txt' within the 'twoFA' folder.
+    with open(resource_path('TwoFA/key.txt'), 'r') as file:  # Opens and Reads 'key.txt' within the 'TwoFA' folder.
         KEY = file.read()
     return KEY  # Returns 'KEY' as a string variable.
 
@@ -24,7 +34,7 @@ def authentication_setup():
 
     KEY = retrieve_key()  # Retrieves key from 'key.txt' file as a string.
     uri = pyotp.totp.TOTP(KEY).provisioning_uri(name = "Lucas Spitzer", issuer_name = "Password Manager")  # Provides params to create a URI for 2FA App.
-    qrcode.make(uri).save("twoFA/pyotp-qrcode.png")  # Creates 'pyotp-qrcode.png' file to be scanned using Google Authenticator App.
+    qrcode.make(uri).save(resource_path("TwoFA/pyotp-qrcode.png"))  # Creates 'pyotp-qrcode.png' file to be scanned using Google Authenticator App.
 
 
 def verify_2FA_code(code):
@@ -38,8 +48,8 @@ def verify_2FA_code(code):
 def main():
     ''' This file is setup so the user can run this function once, scan the QR code, and utilize the Google Authenticator on another device. '''
 
-    generate_key()  # Generates 2FA key as 'key.txt' in the 'twoFA' folder.
-    authentication_setup()  # Creates QR code as 'pyotp-qrcode.png' in the 'twoFA' folder.
+    generate_key()  # Generates 2FA key as 'key.txt' in the 'TwoFA' folder.
+    authentication_setup()  # Creates QR code as 'pyotp-qrcode.png' in the 'TwoFA' folder.
 
 
 if __name__=="__main__":
